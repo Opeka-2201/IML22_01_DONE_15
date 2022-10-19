9f908ba6f714d4536223dbf96cb5bc449fedd9fc
Opeka-2201 <65912187+Opeka-2201@users.noreply.github.com>
2022-10-19 10:37:30 +0200

    dt + knn start

--- code/dt.py
+++ code/dt.py
@@ -10,14 +10,46 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 from data import make_dataset1, make_dataset2
+from plot import plot_boundary
 from sklearn.tree import DecisionTreeClassifier
 
+def test_depth(depth, n_samples, n_fit):
+  n_tests = 5
+  results = np.zeros(5)
 
-# (Question 1)
+  for i in range(n_tests):
+    test = 0.
+    data = make_dataset2(n_samples)
+    data_points = data[0]
+    data_colors = data[1]
 
-# Put your funtions here
-# ...
+    dt = DecisionTreeClassifier(max_depth=depth)
+    dt.fit(X=data_points[:n_fit+1], y=data_colors[:n_fit+1])
+    sample_colors = data_colors[n_fit+1:]
+    predicted_colors = dt.predict(X=data_points[n_fit+1:])
+    
+    for j in range(len(sample_colors)):
+      if sample_colors[j] == predicted_colors[j]:
+        test+=1
 
+    results[i] = test/(n_samples-n_fit)
+  
+  print("Results for depth = " + str(depth) + ":")
+  print("Mean : " + str(results.mean()))
+  print("Std : " + str(results.std()) + "\n")
 
 if __name__ == "__main__":
-    pass # Make your experiments here
+  depths = [1,2,4,8,None]
+  sample_size = 1500
+  fit_size = 1200
+  data = make_dataset2(n_points=sample_size)
+  data_points = data[0]
+  data_colors = data[1]
+
+  for depth in depths:
+    dt = DecisionTreeClassifier(max_depth=depth)
+    fitted = dt.fit(X=data_points[:fit_size+1], y=data_colors[:fit_size+1])
+    f_name = "figs/dt/dt_" + str(depth)
+    f_title = "Decision tree with max_depth = " + str(depth)
+    plot_boundary(f_name, fitted, data_points, data_colors, title=f_title)
+    test_depth(depth, sample_size, fit_size)
--- code/knn.py
+++ code/knn.py
@@ -11,13 +11,45 @@ from matplotlib import pyplot as plt
 
 from data import make_dataset1, make_dataset2
 from sklearn.neighbors import KNeighborsClassifier
+from plot import plot_boundary
 
+def test_neighbor(neighbor, n_samples, n_fit):
+  n_tests = 5
+  results = np.zeros(5)
 
-# (Question 2)
+  for i in range(n_tests):
+    test = 0.
+    data = make_dataset2(n_samples)
+    data_points = data[0]
+    data_colors = data[1]
 
-# Put your funtions here
-# ...
+    knn = KNeighborsClassifier(n_neighbors=neighbor)
+    knn.fit(X=data_points[:n_fit+1], y=data_colors[:n_fit+1])
+    sample_colors = data_colors[n_fit+1:]
+    predicted_colors = knn.predict(X=data_points[n_fit+1:])
+    
+    for j in range(len(sample_colors)):
+      if sample_colors[j] == predicted_colors[j]:
+        test+=1
 
+    results[i] = test/(n_samples-n_fit)
+  
+  print("Results for n_neighbors = " + str(neighbor) + ":")
+  print("Mean : " + str(results.mean()))
+  print("Std : " + str(results.std()) + "\n")
 
 if __name__ == "__main__":
-    pass # Make your experiments here
+  neighbors = [1,5,25,125,625,1200]
+  sample_size = 1500
+  fit_size = 1200
+  data = make_dataset2(n_points=sample_size)
+  data_points = data[0]
+  data_colors = data[1]
+
+  for neighbor in neighbors:
+    knn = KNeighborsClassifier(n_neighbors=neighbor)
+    fitted = knn.fit(X=data_points[:fit_size+1], y=data_colors[:fit_size+1])
+    f_name = "figs/knn/knn_" + str(neighbor)
+    f_title = "K-nearest neighbor with n_neighbors = " + str(neighbor)
+    plot_boundary(f_name, fitted, data_points, data_colors, title=f_title)
+    test_neighbor(neighbor, sample_size, fit_size)
