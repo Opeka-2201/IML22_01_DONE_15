1a59757290f8e691a9ab61d7391e50185e26471c
Romain Lambermont <lambermontr@gmail.com>
2022-10-21 14:13:19 +0200

    qda a zero

--- code/qda.py
+++ code/qda.py
@@ -6,22 +6,12 @@ Project 1 - Classification algorithms
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from statistics import covariance
-from data import make_dataset1
-from plot import plot_boundary
 import numpy as np
 
 from sklearn.base import BaseEstimator, ClassifierMixin
 
 
-
 class QuadraticDiscriminantAnalysis(BaseEstimator, ClassifierMixin):
-
-    covariance_matrix_1 = np.array([])
-    covariance_matrix_2 = np.array([])
-    classes = []
-    mu = []
-    pi = []
 
     def fit(self, X, y, lda=False):
         """Fit a linear discriminant analysis model using the training set
@@ -51,32 +41,9 @@ class QuadraticDiscriminantAnalysis(BaseEstimator, ClassifierMixin):
 
         self.lda = lda
 
-        samples = np.hstack((X, np.asarray([y]).T))
-
-        dict = {}
-
-        for sample in samples:
-            if sample[-1] in dict:
-              dict[sample[-1]][0] += 1
-              dict[sample[-1]][1] = np.add(dict[sample[-1]][1], sample[0:-1])
-            else:
-              dict[sample[-1]] = [1, np.array(sample[0:-1])]
-
-        for key in dict:
-            dict[key][1] = np.divide(dict[key][1], dict[key][0])
-            dict[key][0] = dict[key][0]/y.shape[0]
-
-            dict_sort = sorted(dict.items(), key = lambda x:x[0])
-
-            for entry in dict_sort:
-              self.classes.append(entry[0])
-              self.pi.append(entry[1][0])
-              self.mu.append(entry[1][1])
-            
-            if self.lda:
-              self.covariance_matrix_1 = np.cov(X, rowvar=False)
-            else:
-              pass
+        # ====================
+        # TODO your code here.
+        # ====================
 
         return self
 
@@ -94,16 +61,11 @@ class QuadraticDiscriminantAnalysis(BaseEstimator, ClassifierMixin):
             The predicted classes, or the predict values.
         """
 
-        probas = self.predict_proba(X)
-        y = np.zeros(np.asarray(X).shape[0])
-        for i, _ in enumerate(X,0):
-          y[i] = self.classes[np.argmax(probas[i])]
+        # ====================
+        # TODO your code here.
+        # ====================
 
-        return y
-
-    def gauss(self, k, x):
-      return np.divide(np.exp(-1/2*(x-self.mu[self.classes.index(k)])@np.linalg.inv(self.covariance_matrix_1)@(x-self.mu[self.classes.index(k)]).T), 
-                        np.power(2*np.pi,x.ndim/2)*np.sqrt(np.linalg.det(self.covariance_matrix_1)))
+        pass
 
     def predict_proba(self, X):
         """Return probability estimates for the test data X.
@@ -120,27 +82,13 @@ class QuadraticDiscriminantAnalysis(BaseEstimator, ClassifierMixin):
             by lexicographic order.
         """
 
-        probas = np.zeros([np.asarray(X).shape[0], np.asarray(self.classes).shape[0]])
+        # ====================
+        # TODO your code here.
+        # ====================
 
-        for i, x in enumerate(X,0):
-          for j, k in enumerate(self.classes, 0):
-            sum = 0
-            for l in self.classes:
-              sum += self.gauss(self.classes.index(l), x) * self.pi[self.classes.index(l)]
+        pass
 
-            probas[i,j] = self.gauss(self.classes.index(k), x) * self.pi[self.classes.index(l)] / sum
-
-        return probas
 
 if __name__ == "__main__":
-  sample_size = 1500
-  fit_size = 1200
-  data = make_dataset1(n_points=sample_size)
-  data_points = data[0]
-  data_colors = data[1]
-
-  lda = QuadraticDiscriminantAnalysis()
-  fitted = lda.fit(X=data_points[:fit_size+1], y=data_colors[:fit_size+1], lda=True)
-  f_name = "figs/lda"
-  f_title = "LDA"
-  plot_boundary(f_name, fitted, data_points, data_colors, title=f_title)
+    from data import make_data
+    from plot import plot_boundary
